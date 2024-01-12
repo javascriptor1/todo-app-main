b7fe73eed2aeb74667b301ae193798ebde63b62e
Mohammed Fakih <mhd.fakih@gmail.com>
2024-01-12 11:20:27 +0300

    cloning filter div done when screen resize

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-'use strict'
+"use strict";
+const middleSatusDiv = document
+  .querySelector(".status-div__middle-div")
+  .cloneNode(true);
 
-const middleSatusDiv = document.querySelector('.status-div__middle-div').cloneNode(true)
-const desktopStatusDiv = document.querySelector('.middle-div')
-middleSatusDiv.classList.remove('status-div__middle-div')
-middleSatusDiv.classList.add('status-div__middle-div--flex')
-desktopStatusDiv.appendChild(middleSatusDiv)
+function initializeFilterDiv() {
+  if (window.innerWidth >= 768) {
+    const desktopStatusDiv = document.querySelector(".middle-div");
+    // we can update the class of cloned element in two different methods:
+    // - First method:
+    // middleSatusDiv.classList.remove('status-div__middle-div')
+    // middleSatusDiv.classList.add('status-div__middle-div--flex')
+    // - Second method by overriding original class name:
+    middleSatusDiv.className = "status-div__middle-div--flex";
+    desktopStatusDiv.appendChild(middleSatusDiv);
+  }
+}
+
+function removeFilterDiv() {
+  if (window.innerWidth < 768) {
+    if (middleSatusDiv) {
+      middleSatusDiv.remove();
+    }
+  }
+}
+
+window.addEventListener("resize", initializeFilterDiv);
+window.addEventListener("resize", removeFilterDiv);
+
+initializeFilterDiv();
