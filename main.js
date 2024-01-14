8974c6cf2eb7bcd564c5284fbfe916612a2a7e55
Mohammed Fakih <mhd.fakih@gmail.com>
2024-01-14 22:21:35 +0300

    drag and drop done

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 "use strict";
+// ************************************************************************************/
+//                                  Application selectors
+//**************************************************************************************/
 const middleStatusDiv = document
   .querySelector(".status-div__middle-div")
   .cloneNode(true);
@@ -8,6 +11,13 @@ const todoList = document.querySelector(".todosList");
 let todoArray = [];
 const counterSpanEl = document.querySelector("[data-counter]");
 const emptyMsg = document.querySelector(".empty-message");
+const clearCompleteBtn = document.querySelector(".status-div button");
+const filterBtns = document.querySelectorAll(".status-div__middle-div button");
+let filterBtnsLargeScreens;
+
+// ************************************************************************************/
+//                                  Application functions
+//**************************************************************************************/
 
 function initializeFilterDiv() {
   if (window.innerWidth >= 768) {
@@ -19,6 +29,9 @@ function initializeFilterDiv() {
     // - Second method by overriding original class name using className property:
     middleStatusDiv.className = "status-div__middle-div--flex";
     desktopStatusDiv.appendChild(middleStatusDiv);
+    filterBtnsLargeScreens = document.querySelectorAll(
+      ".status-div__middle-div--flex button"
+    );
   }
 }
 
@@ -30,21 +43,18 @@ function removeFilterDiv() {
   }
 }
 
-window.addEventListener("resize", initializeFilterDiv);
-window.addEventListener("resize", removeFilterDiv);
-
 initializeFilterDiv();
-
-form.addEventListener("submit", addTodo);
 
 function displayTodo(todoArray) {
   let todoText = "";
   todoArray.forEach((todo, index) => {
     todoText += `
-        <li class="todo-item" data-id="${index}">
+        <li class="todo-item" data-id="${index}" draggable="true">
             <div class="checkbox-div">
                 <label for="checkbox${index}" class="label">checkbox</label>
-                <input type="checkbox" id="checkbox${index}" class="checkbox-round" />
+                <input type="checkbox" id="checkbox${index}" class="checkbox-round" ${
+      todo.todoStatus ? " checked" : ""
+    }/>
                 <p class="todo-text">${todo.todoItem}</p>
             </div>
             <button type="button">
@@ -59,11 +69,21 @@ function displayTodo(todoArray) {
 }
 
 function addTodo(e) {
+  let duplicateStatus = false;
   e.preventDefault();
   if (todoInputEl.value === "") {
     alert("Enter todo...");
     return;
   }
+  todoArray.forEach((element) => {
+    if (element.todoItem === todoInputEl.value) {
+      alert("Todo already exists..🛑. Try something else");
+      duplicateStatus = true;
+      todoInputEl.value = "";
+      return;
+    }
+  });
+  if (duplicateStatus === true) return;
 
   let todoItem = todoInputEl.value.trim();
   let todoStatus = false;
@@ -79,20 +99,20 @@ function addTodo(e) {
   getTodoCount();
 }
 
-function handleTodo(e) {
-  if (e.target.nodeName === "IMG") {
-    const removedItem =
-      e.target.parentNode.previousElementSibling.children[2].textContent;
-    todoArray = todoArray.filter((todo) => {
-      if (todo.todoItem !== removedItem) return todo;
-    });
+function removeTodo(e) {
+  if (e.target.nodeName !== "IMG") return;
+  const removedItem =
+    e.target.parentNode.previousElementSibling.children[2].textContent;
+  todoArray = todoArray.filter((todo) => {
+    if (todo.todoItem !== removedItem) return todo;
+  });
+
+  if (window.confirm(`Are you sure you want to delete: ${removedItem}`)) {
     e.target.parentNode.parentNode.remove();
     handleEmptyMessage();
     getTodoCount();
-  }
+  } else return;
 }
-
-todoList.addEventListener("click", handleTodo);
 
 function handleEmptyMessage() {
   if (todoList.children.length > 0) {
@@ -120,7 +140,94 @@ function updateTodoStatus(event) {
   getTodoCount();
 }
 
+function clearCompleted() {
+  todoArray = todoArray.filter((element) => element.todoStatus === false);
+  displayTodo(todoArray);
+  getTodoCount();
+}
+
+function filterTodo(event) {
+  if (event.target.textContent === "All") displayTodo(todoArray);
+
+  if (event.target.textContent === "Active") {
+    const itemsCount = todoArray.filter(
+      (element) => element.todoStatus === false
+    ).length;
+    if (itemsCount === 0) {
+      alert("No Active items found 🤚");
+    } else {
+      displayTodo(todoArray.filter((element) => element.todoStatus === false));
+    }
+  }
+  if (event.target.textContent === "Completed") {
+    const itemsCount = todoArray.filter(
+      (element) => element.todoStatus === true
+    ).length;
+
+    if (itemsCount === 0) {
+      alert("No completed items found 🤚");
+    } else {
+      displayTodo(todoArray.filter((element) => element.todoStatus === true));
+    }
+  }
+}
+
+// drag and drop functionality
+function dragStart(e) {
+  e.dataTransfer.setData("dragStartIndex", e.target.getAttribute("data-id"));
+}
+
+function dragEnter(e) {
+  e.target.classList.add("draggable");
+}
+
+function dragLeave(e) {
+  e.target.classList.remove("draggable");
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.preventDefault();
+  const dragStartIndex = e.dataTransfer.getData("dragStartIndex");
+  const dragEndIndex = parseInt(e.target.getAttribute("data-id"), 10);
+  console.log(dragEndIndex);
+  swapTodos(dragStartIndex, dragEndIndex);
+  e.target.classList.remove("draggable");
+}
+
+function swapTodos(fromIndex, toIndex) {
+  const sourceEl = todoList.querySelector(`[data-id= "${fromIndex}"]`);
+  const targetEl = todoList.querySelector(`[data-id= "${toIndex}"]`);
+  console.log(sourceEl, targetEl);
+  // swap the source and target element by array destructuring method
+  [todoArray[fromIndex], todoArray[toIndex]] = [
+    todoArray[toIndex],
+    todoArray[fromIndex],
+  ];
+  displayTodo(todoArray);
+}
+
+// application functionality [event listeners]
+window.addEventListener("resize", initializeFilterDiv);
+window.addEventListener("resize", removeFilterDiv);
+form.addEventListener("submit", addTodo);
+todoList.addEventListener("click", removeTodo);
+todoList.addEventListener("dragstart", dragStart);
+todoList.addEventListener("dragenter", dragEnter);
+todoList.addEventListener("dragleave", dragLeave);
+todoList.addEventListener("dragover", dragOver);
+todoList.addEventListener("drop", drop);
 todoList.addEventListener("change", updateTodoStatus);
+clearCompleteBtn.addEventListener("click", clearCompleted);
+filterBtns.forEach((element) => {
+  element.addEventListener("click", filterTodo);
+});
+filterBtnsLargeScreens.forEach((element) => {
+  element.addEventListener("click", filterTodo);
+});
 
 function init() {
   todoInputEl.focus();
